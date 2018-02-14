8e84879c68fafae679108cd5fd377b146825adae
akellaX <dodonova.anna76@gmail.com>
2018-02-14 23:41:51 +0300

    dd

--- progress_bar.js
+++ progress_bar.js
@@ -1,20 +1,20 @@
 window.onload = function () {
 	initCheckbox();
 	input = document.getElementById("percent");
-	input.addEventListener("change", function(){
+	input.addEventListener("change", function () {
 		var val = parseInt(this.value);
-		if(isNaN(val))
+		if (isNaN(val))
 			alert("wrong value");
 		else {
 			var radius = document.getElementById("b_circle").getAttribute("r");
-			var k = Math.PI * (radius*2);
+			var k = Math.PI * (radius * 2);
 			if (val < 0) {
 				val = 0;
 			}
 			if (val > 100) {
 				val = 100;
 			}
-			var change = ((100-val)/100)*k;
+			var change = ((100 - val) / 100) * k;
 			document.getElementById("s_circle").style.strokeDashoffset = change;
 			console.log(change);
 
@@ -23,22 +23,16 @@ window.onload = function () {
 }
 
 function initCheckbox() {
-	document.getElementById("animation").addEventListener("change", function() {
-		if(this.checked)
-			{
+	document.getElementById("animation").addEventListener("change", function () {
+		if (this.checked) {
 			document.getElementById("loader").style.animationPlayState = "running";
-			}
-		else
+		} else
 			document.getElementById("loader").style.animationPlayState = "paused";
 	})
-		document.getElementById("hide").addEventListener("change", function() {
-		if(this.checked)
-			{
+	document.getElementById("hide").addEventListener("change", function () {
+		if (this.checked) {
 			document.getElementById("loader").style.visibility = "hidden";
-			}
-		else
+		} else
 			document.getElementById("loader").style.visibility = "visible";
 	})
 }
-
-
