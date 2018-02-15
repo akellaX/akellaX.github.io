725e3b7a46dcd9338fdec8f4ca416a4291803e6c
akellaX <dodonova.anna76@gmail.com>
2018-02-15 04:23:05 +0300

    465

--- progress_bar.js
+++ progress_bar.js
@@ -3,7 +3,7 @@ function Progress_bar() {
 
 	this.progress_block = document.getElementsByClassName("progress-bar")[0];
 
-	this.progress_block.innerHTML = "<div class='container'><svg class='loader' width='200' height='200'><circle class = 'b_circle' r = '90' cx = '100' cy = '100' fill = 'transparent' stroke-dasharray='565.48' stroke-dashoffset='0'></circle><circle class = 's_circle bar' r='90' cx='100' cy='100' fill='transparent' stroke-dasharray='565.48' stroke-dashoffset='0'></circle></svg></div>";
+	this.progress_block.innerHTML = "<svg class='loader' width='200' height='200'><circle class = 'b_circle' r = '90' cx = '100' cy = '100' fill = 'transparent' stroke-dasharray='565.48' stroke-dashoffset='0'></circle><circle class = 's_circle bar' r='90' cx='100' cy='100' fill='transparent' stroke-dasharray='565.48' stroke-dashoffset='0'></circle></svg>";
 
 
 	this.loader = document.getElementsByClassName("loader")[0];
